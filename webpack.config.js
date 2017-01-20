a1c61e379dc2a7c9e3cd4fd7d9e00f842c959619
Sebastian <sebastian@konsulterna.nu>
2017-01-20 14:07:46 +0100

    configure umd and cjs builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,43 @@
-// Modules
-const { resolve } = require('path')
 const webpack = require('webpack')
 
-const context = resolve(__dirname, 'src')
 
-// Webpack config
 module.exports = {
-	context,
-	entry: [
-		'react-hot-loader/patch',
-		'webpack-dev-server/client?http://localhost:8080',
-		'webpack/hot/only-dev-server',
-		'./index.js'
-	],
+
 	output: {
-		filename: 'bundle.js',
-		path: resolve(__dirname, 'dist'),
-		pathinfo: true,
+		library: 'ReactTabby',
+		libraryTarget: 'umd',
 	},
-	devtool: 'inline-source-map',
-	devServer: {
-		hot: true,
-		contentBase: resolve(__dirname, 'public'),
-		filename: 'bundle.js',
-		inline: true,
-	},
+
+	externals: [
+		{
+			react: {
+				root: 'React',
+				commonjs2: 'react',
+				commonjs: 'react',
+				amd: 'react',
+			}
+		}
+	],
+
 	module: {
-		rules: [
+		loaders: [
 			{
-				test: /\.js$/,
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							plugins: [
-								'transform-react-jsx',
-								[
-									'react-css-modules',
-									{
-										context,
-										webpackHotModuleReloading: true,
-									}
-								]
-							]
-						}
-					}
-				],
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{
-						loader: 'style-loader',
-					},
-					{
-						loader: 'css-loader?importLoader=1&modules&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-					}
-				]
+				test: /\.js$/, 
+				exclude: /node_modules/, 
+				loader: 'babel-loader' 
 			}
 		]
 	},
+
+	node: {
+		Buffer: false
+	},
+
 	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NamedModulesPlugin(),
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+		})
 	]
+
 }
